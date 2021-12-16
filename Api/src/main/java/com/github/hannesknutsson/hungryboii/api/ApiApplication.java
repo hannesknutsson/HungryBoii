711c30e9a28e497ba3a7670255cb05b669795c8e
Martin Moberg <martin.moberg@fortnox.se>
2021-12-16 15:08:32 +0100

    push all teh things

--- Api/src/main/java/com/github/hannesknutsson/hungryboii/api/ApiApplication.java
+++ Api/src/main/java/com/github/hannesknutsson/hungryboii/api/ApiApplication.java
@@ -6,11 +6,15 @@ import com.github.hannesknutsson.hungryboii.api.dataclasses.restaurants.implemen
 import com.github.hannesknutsson.hungryboii.api.dataclasses.restaurants.implementations.VidaArena;
 import com.github.hannesknutsson.hungryboii.api.managers.implementations.RestaurantManager;
 import com.github.hannesknutsson.hungryboii.api.workers.MenuGatherer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ApiApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
@@ -19,6 +23,8 @@ public class ApiApplication {
         RestaurantManager.getInstance().register(new Ostergatan());
         RestaurantManager.getInstance().register(new Kok11());
         RestaurantManager.getInstance().register(new VidaArena());
+
+        LOG.info("hello");
 
         MenuGatherer.startGathering();
     }
