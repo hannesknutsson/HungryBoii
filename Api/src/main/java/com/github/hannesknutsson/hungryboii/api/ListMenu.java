be45e942125337ab0d53ea80deb6873742a976a9
Patrik Lindberg <patrik.lindberg@fortnox.se>
2021-12-17 10:49:56 +0100

    Fix some markdown

--- Api/src/main/java/com/github/hannesknutsson/hungryboii/api/ListMenu.java
+++ Api/src/main/java/com/github/hannesknutsson/hungryboii/api/ListMenu.java
@@ -48,10 +48,10 @@ public class ListMenu {
         Message message = Message.home(
                 markdownSection("*Todays lunch* :fork_and_knife:"),
                 divider(),
-                markdownSection("*<https://www.kok11.se/dagens-lunch/%7CKök 11>*\nOpen 11:30-13:30  |  Price 115:-  |  Take-away/kuponger 105:-\n\n\t* Havets Wallenbergare med hummersås, juliennegrönsaker och potatisbakelse (LF)\n\t* Pulled pork med ugnsrostad klyftpotatis, rödkålsslaw och naanbröd (LF)\n\t* Vegetarisk kebab med pitabröd, vitlöksdressing, syrad lök och krispig sallad"),
-                markdownSection("*<https://www.vaxjolakers.se/mat-dryck/lunchmeny%7CVida Arena>*\nOpen 11:30 - 14:00  |  Price 115:-  |  Arenakort 105:-\n\n\t* Helstekt karré med pepparsky och potatisgratäng (G)\n\t* Lasagne med lax och bladspenat\n\t* Fried rice med bbq-marinerad tofu (VGL)"),
-                markdownSection("*<https://ostergatansrestaurang.se/lunch/%7CÖstergatan>*\nOpen 11:00 - 13:30  |  Price 99:-  |  Östergatankort 94:-\n\n\t* Fläskschnitzel med stekt potatis, gröna ärter & dragonsås\n\t* Janssonsfrestelse med stekt prinskorv & senap\n\t* Grönsaksfylld paprika med Quornfilé, bulgur & örtdressing "),
-                markdownSection("*<https://www.restaurangfuturum.se/dagens-lunch%7CFuturum>*\nOpen 11:00 - 13:30  |  Price 98:-  |  Take-away 98:-\n\n\t* Wallenbergare med potatismos, lingon & rödvinssås\n\t* Rotfruktsgratäng med stekt quornfilé & örtcreme"),
+                markdownSection(":one:  *<https://www.kok11.se/dagens-lunch/|Kök 11>*\nOpen 11:30-13:30  |  Price 115:-  |  Take-away/kuponger 105:-\n\n\t* Havets Wallenbergare med hummersås, juliennegrönsaker och potatisbakelse (LF)\n\t* Pulled pork med ugnsrostad klyftpotatis, rödkålsslaw och naanbröd (LF)\n\t* Vegetarisk kebab med pitabröd, vitlöksdressing, syrad lök och krispig sallad"),
+                markdownSection(":two:  *<https://www.vaxjolakers.se/mat-dryck/lunchmeny|Vida Arena>*\nOpen 11:30 - 14:00  |  Price 115:-  |  Arenakort 105:-\n\n\t* Helstekt karré med pepparsky och potatisgratäng (G)\n\t* Lasagne med lax och bladspenat\n\t* Fried rice med bbq-marinerad tofu (VGL)"),
+                markdownSection(":three:  *<https://ostergatansrestaurang.se/lunch/|Östergatan>*\nOpen 11:00 - 13:30  |  Price 99:-  |  Östergatankort 94:-\n\n\t* Fläskschnitzel med stekt potatis, gröna ärter & dragonsås\n\t* Janssonsfrestelse med stekt prinskorv & senap\n\t* Grönsaksfylld paprika med Quornfilé, bulgur & örtdressing "),
+                markdownSection(":four:  *<https://www.restaurangfuturum.se/dagens-lunch|Futurum>*\nOpen 11:00 - 13:30  |  Price 98:-  |  Take-away 98:-\n\n\t* Wallenbergare med potatismos, lingon & rödvinssås\n\t* Rotfruktsgratäng med stekt quornfilé & örtcreme"),
                 Blocks.actions(buttonElement())
         );
         return new Gson().toJson(message);
